5d4987423d3049862af0fb8d352264c271ac0140
Sunil Shrestha <sthasunil664@gmail.com>
2026-02-19 10:34:19 +0530

    This is just check

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+app.get("/say", (req, res) =>{
+    const message = req.query.message || "Hello!";
+    res.send(`You said: ${message}`);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
